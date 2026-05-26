1f330e036cb98ec8f7f4bfc8eab62a8dc50d6380
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-05-26 11:56:27 +0200

    PasswordResetController : séparation GET/POST resetPassword

--- app/controllers/PasswordResetController.php
+++ app/controllers/PasswordResetController.php
@@ -38,48 +38,48 @@ class PasswordResetController{
         }
     }
 
-    public function resetPassword(){
-        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-            Auth::verifyCsrfToken();
-            $token = $_POST['token'];
-            $user = $this->users->findByResetToken($token);
-            if(!$user){
-                $error = "Cet utilisateur n'existe pas ! Merci de créer un compte valide ! ";
-                header('location: /auth/resetPassword?error=' . urlencode($error));
-                exit();
-            }
-            if($user['reset_token_expires_at'] < date('Y-m-d H:i:s', time())){
-                $error = "Votre token a expiré !";
-                header('location: /auth/resetPassword?error=' . urlencode($error));
-                exit();
-            }else{
-                if($_POST['password'] !== $_POST['password_confirm']){
-                    $error = "Les deux mots de passe ne correspondent pas !";
-                    header('Location: /auth/resetPassword?error=' . urlencode($error));
-                    exit();
-                }
-                if(strlen($_POST['password']) < 14){
-                    $error = "Le mot de passe ne contient le nombre minimum de 14 caractères ! Veuillez recommencer svp .";
-                    header('location:/auth/resetPassword?error=' . urlencode($error));
-                    exit();
-                }
 
-                $hashedPassword= password_hash($_POST['password'], PASSWORD_BCRYPT);
-                $id = $user['id'] ;
-                $this->users->updatePassword($id , $hashedPassword);
-                $this->users->clearResetToken($id);
-                $email = $user['email'];
-                $subject = "Mot de passe réinitialsé !";
-                $body = "Vous avez réinitialsé votre mot de passe avec succès ! ";
-                $this->mailService->sendEmail($email,$subject,$body);
-                $succesMessage = "Vous avez réinitialsé votre mot de passe avec succès !";
-                header('location: /auth/login?success=' . urlencode($succesMessage));
+    public function resetPasswordForm(){
+        $token = $_GET['token'] ?? '';
+        Auth::generateCsrfToken();
+        require_once __DIR__ . '/../views/auth/resetPassword.php';
+    }
+    public function resetPassword(){
+        Auth::verifyCsrfToken();
+        $token = $_POST['token'];
+        $user = $this->users->findByResetToken($token);
+        if(!$user){
+            $error = "Cet utilisateur n'existe pas ! Merci de créer un compte valide ! ";
+            header('location: /auth/reset-password?error=' . urlencode($error));
+            exit();
+        }
+        if($user['reset_token_expires_at'] < date('Y-m-d H:i:s', time())){
+            $error = "Votre token a expiré !";
+            header('location: /auth/reset-password?error=' . urlencode($error));
+            exit();
+        }else{
+            if($_POST['password'] !== $_POST['password_confirm']){
+                $error = "Les deux mots de passe ne correspondent pas !";
+                header('Location: /auth/reset-password?error=' . urlencode($error));
                 exit();
             }
-        }else{
-            $token = $_GET['token'] ?? '';
-            Auth::generateCsrfToken();
-            require_once __DIR__ . '/../views/auth/resetPassword.php';
+            if(strlen($_POST['password']) < 14){
+                $error = "Le mot de passe ne contient le nombre minimum de 14 caractères ! Veuillez recommencer svp .";
+                header('location:/auth/reset-password?error=' . urlencode($error));
+                exit();
+            }
+
+            $hashedPassword= password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $id = $user['id'] ;
+            $this->users->updatePassword($id , $hashedPassword);
+            $this->users->clearResetToken($id);
+            $email = $user['email'];
+            $subject = "Mot de passe réinitialsé !";
+            $body = "Vous avez réinitialsé votre mot de passe avec succès ! ";
+            $this->mailService->sendEmail($email,$subject,$body);
+            $succesMessage = "Vous avez réinitialsé votre mot de passe avec succès !";
+            header('location: /auth/login?success=' . urlencode($succesMessage));
+            exit();
         }
     }
 }

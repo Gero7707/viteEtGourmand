0c96c7279af32fbd147d1049a2cf0fb8cb93829b
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-05-26 15:39:21 +0200

    Formulaire de création de compte dans register.php

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -6,12 +6,19 @@ require_once __DIR__ . '/../../views/layout/header.php';
         <?= Auth::csrfField() ?>
         <label for="email">Email</label><br>
         <input type="email" name="email" id="email" required><br>
-        <label for="pseudo">Pseudonyme</label><br>
-        <input type="text" name="pseudo" id="pseudo"><br>
+
+        <label for="nom">Nom</label><br>
+        <input type="text" name="nom" id="nom" required><br>
+
+        <label for="prenom">Prénom</label><br>
+        <input type="text" name="prenom" id="prenom" required><br>
+
         <label for="password">Mot de passe</label><br>
         <input type="password" name="password" id="password" required><br>
+        
         <label for="password_confirm">Confirmer le mot de passe</label><br>
-        <input type="password" name="password_confirm" id="password_confirm"><br>
+        <input type="password" name="password_confirm" id="password_confirm" required><br>
+
         <button type="submit">Créer votre compte</button>
     </form>
     <?php if ($_GET['error'] ?? null): ?>

a6d429551b07392a82c74988aab400fa80c56cc3
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-06-05 14:48:27 +0200

    import cdn fontawsome et bootstrap , ainsi que les fonts google fonts , config d'apache.conf pour les csp et authoriser les import externes , charte graphique dans style.css

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Manrope:wght@200..800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a5f2a52ad7.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/assets/css/style.css">
     <title>Vite et gourmand</title>
 </head>
 <body>
+    
     <header>
+        <p class="titre">Vite et Gourmand</p>
         <ul>
             <li><a href="/contact">Contact</a></li>
         </ul>

bf37e7d90b78f148207a580e48f6b10ae7fc0564
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-06-08 15:20:38 +0200

    vue updatePlat.php affichage du formulaire et message d'erreur js

--- app/views/employe/updatePlat.php
+++ app/views/employe/updatePlat.php
@@ -1,43 +1,56 @@
 <?php
-$pageSpecificCss = 'style.css';
+$pageSpecificCss = ['style.css' , 'formulaire.css'];
 require_once __DIR__ . '/../../views/layout/header.php';
 ?>
 <main>
-    <a href="/plats">Retour aux plats</a><br>
-    <?php if ($_GET['error'] ?? null): ?>
-        <p class="error-message mt-1"><?= htmlspecialchars($_GET['error']) ?></p>
-    <?php endif ?>
     <?php if ($_GET['success'] ?? null): ?>
         <p class="success-message mt-1"><?= htmlspecialchars($_GET['success']) ?></p>
     <?php endif ?>
-    <hr>
-    <h4>Modifier un plat</h4>
-    <form action="/plats/edit/<?= htmlspecialchars($plats['plat_id']) ?>" method="POST" enctype="multipart/form-data">
-        <?= Auth::csrfField() ?>
-        <label for="titre_plat">Titre du plat </label><br>
-        <input type="text" name="titre_plat" id="titre_plat" value="<?= htmlspecialchars($plats['titre_plat']) ?>" required><br>
+    <div class="d-flex justify-content-center">
+        <div class="d-flex flex-column text-center form-contact">
+            <h4 class="text-center mt-4 mb-5">Modifier un plat</h4>
+            <p class="error-message mt-1 text-center"></p><br>
+            <form action="/plats/edit/<?= htmlspecialchars($plats['plat_id']) ?>" method="POST" enctype="multipart/form-data" class="text-center">
+                <?= Auth::csrfField() ?>
 
-        <label for="type_plat">Type </label><br>
-        <select name="type_plat" id="type_plat" value="<?= htmlspecialchars($plats['type_plat']) ?>" required>
-            <option value="entree"<?= htmlspecialchars($plats['type_plat']) == 'entree' ? 'selected' : '' ?>>Entrée</option>
-            <option value="plat" <?= htmlspecialchars($plats['type_plat']) == 'plat' ? 'selected' : '' ?>>Plat</option>
-            <option value="dessert" <?= htmlspecialchars($plats['type_plat']) == 'dessert' ? 'selected' : '' ?>>Dessert</option>
-        </select><br>
+                <label class="mt-3" for="titre_plat">Titre du plat </label><br>
+                <input class="mt-3 mb-3" type="text" name="titre_plat" id="titre_plat" value="<?= htmlspecialchars($plats['titre_plat']) ?>" required><br>
 
-        <label for="allergene">Allergenes</label><br>
-            <?php foreach($allergenes as $allergene) : ?>
-                <input type="checkbox" name="allergenes[]" id="allergene_<?= $allergene['allergene_id'] ?>" value="<?= $allergene['allergene_id'] ?>"  <?= in_array($allergene['allergene_id'], $allergenesIds) ? 'checked' : '' ?>>
-                <label for="allergene_<?= $allergene['allergene_id'] ?>"><?= $allergene['libelle'] ?></label><br>
-            <?php endforeach ?>
+                <div class="cadre-input">
+                    <label class="mt-3" for="type_plat">Type </label><br>
+                    <select name="type_plat" id="type_plat" value="<?= htmlspecialchars($plats['type_plat']) ?>" required>
+                        <option value="entree"<?= htmlspecialchars($plats['type_plat']) == 'entree' ? 'selected' : '' ?>>Entrée</option>
+                        <option value="plat" <?= htmlspecialchars($plats['type_plat']) == 'plat' ? 'selected' : '' ?>>Plat</option>
+                        <option value="dessert" <?= htmlspecialchars($plats['type_plat']) == 'dessert' ? 'selected' : '' ?>>Dessert</option>
+                    </select><br>
+                </div>
 
-        <img src="/<?=htmlspecialchars($plats['chemin_photo']) ?>" alt="" height="50" width="50"><br>
-        <input type="file" name="chemin_photo" id="chemin_photo" ><br>
-        <button type="button" id="btn-showUpload-plat">Changer Image</button><br>
+                <div class="cadre-input">
+                    <label class="mt-4 mb-2" for="allergene">Allergenes</label><br>
+                    <div class="check-allergene m-auto">
+                        <?php foreach($allergenes as $allergene) : ?>
+                            <div class="d-flex flex-start">
+                                <input type="checkbox" name="allergenes[]" id="allergene_<?= $allergene['allergene_id'] ?>" value="<?= $allergene['allergene_id'] ?>"  <?= in_array($allergene['allergene_id'], $allergenesIds) ? 'checked' : '' ?>>
+                                <label for="allergene_<?= $allergene['allergene_id'] ?>"><?= $allergene['libelle'] ?></label><br>
+                            </div>
+                        <?php endforeach ?>
+                    </div>
+                </div>
 
-        <button type="submit">Modifier plat</button>
-    </form>
+                <div class="cadre-input">
+                    <img src="/<?=htmlspecialchars($plats['chemin_photo']) ?>" alt="" height="50" width="50"><br>
+                    <input type="file" name="chemin_photo" id="chemin_photo" ><br>
+                    <button type="button" id="btn-showUpload-plat">Changer Image</button><br>
+                </div>
+
+                <button class="btn-form mt-3 mb-3" type="submit">Modifier plat</button>
+            </form>
+        </div>
+    </div>
+    
 </main>
 <script src="/assets/js/cta.js"></script>
 <?php
+$loadScriptJs = 'form.js';
 require_once __DIR__ . '/../../views/layout/footer.php';
 ?>

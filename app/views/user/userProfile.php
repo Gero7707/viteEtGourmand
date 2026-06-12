3260548e5a13ea90a9fb48b2ce2fcdfad0092bde
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-06-12 21:26:53 +0200

    css vue userProfile.php avec section données pesronnelles et section commandes

--- app/views/user/userProfile.php
+++ app/views/user/userProfile.php
@@ -4,8 +4,6 @@ require_once __DIR__ . '/../../views/layout/header.php';
 ?>
 
 <main>
-    <a href="/">Accueil</a><br>
-
     <?php if ($_GET['error'] ?? null): ?>
         <p class="error-message mt-1"><?= htmlspecialchars($_GET['error']) ?></p>
     <?php endif ?>
@@ -13,24 +11,51 @@ require_once __DIR__ . '/../../views/layout/header.php';
         <p class="success-message mt-1"><?= htmlspecialchars($_GET['success']) ?></p>
     <?php endif ?>
 
-    <p><?= htmlspecialchars($user['prenom'])  ?> , voici vos infos </p><br>
-    <p>Votre email : <?= htmlspecialchars($user['email'])  ?></p><br>
-    <p>Votre Nom : <?= htmlspecialchars($user['nom'])  ?></p><br>
-    <p>Votre Prenom : <?= htmlspecialchars($user['prenom'])  ?></p><br>
-    <p>Votre numéro de téléphone :  <?= htmlspecialchars($user['gsm']?? '' )  ?></p><br>
-    <p>Votre ville :  <?= htmlspecialchars($user['ville'] ?? '' )  ?></p><br>
-    <p>Votre adresse :  <?= htmlspecialchars($user['adresse'] ?? '' )  ?></p><br>
-    <p>Votre code postale :  <?= htmlspecialchars($user['code_postal'] ?? '' )  ?></p><br>
-    <hr>
-    <h4>Vos commandes</h4><br>
-    <?php foreach($commandes as $commande) : ?>
-        <p><?= htmlspecialchars($commande['numero_commande'])  ?></p><br>
-        <p><?= htmlspecialchars($commande['date_commande'])  ?></p><br>
-        <p><?= htmlspecialchars($commande['titre'])  ?></p><br>
-        <p><?= htmlspecialchars($commande['statut'])  ?></p><br>
-        <a href="/commandes/<?= $commande['commande_id'] ?>">Voir commande</a><br>
-    <?php endforeach ?>
-        <a href="/profile/edit">Modifier profil</a>
+    <section class="section-profile-user mb-5">
+        <h5 class="text-center"><?= htmlspecialchars($user['prenom'])  ?> , voici les données que vous avez enregistré sur votre profil : </h5><br>
+        <div class="donnees-user">
+            
+            <p><span>Email</span> :  <?= htmlspecialchars($user['email'])  ?></p><br>
+            <p><span>Nom</span> : <?= htmlspecialchars($user['nom'])  ?></p><br>
+            <p><span>Prenom</span> : <?= htmlspecialchars($user['prenom'])  ?></p><br>
+            <p><span>Téléphone</span> :  <?= htmlspecialchars($user['gsm']?? '' )  ?></p><br>
+            <p><span>Adresse</span> :  <?= htmlspecialchars($user['adresse'] ?? '' )  ?><br><?= htmlspecialchars($user['code_postal'] ?? '' )  ?>  , <?= htmlspecialchars($user['ville'] ?? '' )  ?></p>
+            <hr>
+            <div class="modif-profil">
+                <a href="/profile/edit">Modifier profil</a>
+            </div>
+        </div>
+    </section>
+    <section class="section-commandes">
+        <h5 class="text-center">Vos commandes</h5>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Menu</th>
+                    <th>No commande</th>
+                    <th>Date commande</th>
+                    <th>Statut</th>
+                    <th>Voir</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($commandes as $commande) : ?>
+                    <tr>
+                        <td><?= htmlspecialchars($commande['titre'])  ?></p></td>
+                        <td><?= htmlspecialchars($commande['numero_commande'])  ?></td>
+                        <td><?= date('d/m/Y', strtotime($commande['date_commande']))  ?></td>
+                        <td><?= htmlspecialchars($commande['statut'])  ?></td>
+                        <td>
+                            <a href="/commandes/<?= $commande['commande_id'] ?>"><i class="fa-solid fa-eye"></i></a>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+        
+    </section>
+        
 </main>
 <?php
 require_once __DIR__ . '/../../views/layout/footer.php';

b2e72bc393b23d68ac77d66bf63c5f2ec339ebfc
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-06-15 18:09:57 +0200

    feat: migration données MySQL → MongoDB pour commandes terminées

--- app/models/MongoModel.php
+++ app/models/MongoModel.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../core/MongoDatabase.php';
 class MongoModel{
     
 
-    public function insertCommande(array $data): void {
+public function insertCommande(array $data): void {
         $collection = MongoDatabase::getInstance()->getCollection('commandes');
         $collection->insertOne($data);
     }

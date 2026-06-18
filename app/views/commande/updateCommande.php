55e40464b6a50a4d5aa245e56a035a03d577ad98
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-06-18 16:53:52 +0200

    feat updateCommande adapté

--- app/views/commande/updateCommande.php
+++ app/views/commande/updateCommande.php
@@ -3,12 +3,15 @@ $pageSpecificCss = ['style.css' , 'formulaire.css' , 'layout.css'];
 require_once __DIR__ . '/../../views/layout/header.php';
 ?>
 
-<main class="d-flex justify-content-around">
+<main class="d-flex justify-content-around main-commande">
     <div class="carte-menu-commande">
         <div class="d-flex justify-content-around">
             <div class="d-flex flex-column mt-1">
                 <h5><?= htmlspecialchars($menu['titre']) ?></h5>
-                <p class="theme-carte"><i class="fa-solid fa-square"></i>       &nbsp;<?= htmlspecialchars($menu['regime']) ?></p> 
+                <p>Theme :</p>
+                <p class="theme-carte"><?= htmlspecialchars($menu['theme']) ?></p>
+                <p>Régime :</p>
+                <p class="regime-carte"><?= htmlspecialchars($menu['regime']) ?></p> 
             </div>
             <div class="d-flex flex-column prix-menu mt-1 mb-5">
                 <h6>Prix pour <?= htmlspecialchars($menu['nombre_personne_minimum']) ?> personnes :</h6>

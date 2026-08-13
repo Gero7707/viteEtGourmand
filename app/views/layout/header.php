ecbf2984762be84e45a99ac1b7ebab9c03a87ae0
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-08-13 09:54:36 +0200

    fix: condition d'affichage menu d&roulant header

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -79,9 +79,11 @@
         <?php if(isset($_SESSION['utilisateur_id'])): ?>
             <?php if(($_SESSION['role_id']=== 2 || $_SESSION['role_id'] === 3) && ($pageMenu ?? false)) :?>
                 <div class="dropdown dropdown-entreprise ">
-            <?php else :?>
+            <?php else:?>
                 <div class="dropdown">
             <?php endif ?>
+        <?php elseif(!isset($_SESSION['utilisateur_id']) ) :?>
+            <div class="dropdown">
         <?php endif ?>
         
             <a class="btn  " href="#" role="button" aria-label="Menu de navigation" data-bs-toggle="dropdown" aria-expanded="false">

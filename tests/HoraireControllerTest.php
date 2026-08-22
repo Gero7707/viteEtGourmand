67a025702c958041ea8d695111bb2fae73f79fd6
Vincent Didier Geraghty <vincent-geraghty@live.fr>
2026-08-22 22:08:25 +0200

    test: ajoute les tests unitaires de calculerJoursManquants

---
+++ tests/HoraireControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+require_once __DIR__ . '/../app/controllers/HoraireController.php';
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class HoraireControllerTest extends TestCase
+{
+    #[Test]
+    public function il_detecte_le_jour_manquant(): void
+    {
+        // Arrange : une base où seul le dimanche manque
+        $horaire = [
+            ['jour' => 'Lundi'],
+            ['jour' => 'Mardi'],
+            ['jour' => 'Mercredi'],
+            ['jour' => 'Jeudi'],
+            ['jour' => 'Vendredi'],
+            ['jour' => 'Samedi'],
+        ];
+
+        // Act
+        $resultat = HoraireController::calculerJoursManquants($horaire);
+
+        // Assert : on attend uniquement le dimanche
+        $this->assertSame(['Dimanche'], array_values($resultat));
+    }
+    #[Test]
+    public function il_detecte_aucun_jour_manquant(): void
+    {
+        // Arrange : les 7 jours sont présents, aucun ne manque
+        $horaire = [
+            ['jour' => 'Lundi'],
+            ['jour' => 'Mardi'],
+            ['jour' => 'Mercredi'],
+            ['jour' => 'Jeudi'],
+            ['jour' => 'Vendredi'],
+            ['jour' => 'Samedi'],
+            ['jour' => 'Dimanche'],
+        ];
+
+        // Act
+        $resultat = HoraireController::calculerJoursManquants($horaire);
+
+        // Assert : on attend un tableau vide
+        $this->assertEmpty($resultat);
+    }
+}
